78165bdf808114fcc46adb4b35cd22805f2cd822
grace-jsy <sseo.oee@gmail.com>
2025-07-17 20:11:24 +0900

    반복문 추가

--- src/main/java/kiosklv1/Main.java
+++ src/main/java/kiosklv1/Main.java
@@ -14,24 +14,24 @@ public class Main {
         System.out.println("2. SmokeShack     | ₩ 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
         System.out.println("3. CheeseBurger   | ₩ 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         System.out.println("4. Hamburger      | ₩ 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-        System.out.println("0을 입력하면 종료됩니다");
-        System.out.print("주문을 원하시는 메뉴 번호를 입력해주세요: ");
+        System.out.println("0. Close          | Close |");
+        System.out.print("Your menue Number: ");
 
-        int orderNum = scanner.nextInt();
-        scanner.nextLine();
 
-        if(orderNum == 1) {
-            System.out.println("1. ShackBurger    | ₩ 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        } else if (orderNum == 2) {
-            System.out.println("2. SmokeShack     | ₩ 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        } else if (orderNum == 3) {
-            System.out.println("3. CheeseBurger   | ₩ 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        } else if (orderNum == 4) {
-            System.out.println("4. Hamburger      | ₩ 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-        } else if (orderNum == 0) {
-            System.out.println("종료되었습니다");
+        while (startBtn.equalsIgnoreCase("start")) {
+
+            int inputMenuNum = scanner.nextInt();
+
+            if (inputMenuNum == 0) {
+                System.out.println("Exiting the program.");
+                break;
+            } else if (inputMenuNum < 1 || inputMenuNum > 4) {
+                System.out.println("Wrong Number");
+                System.out.print("Your menu Number: ");
+            } else {
+                System.out.println("Your menu is " + inputMenuNum);
+                break;
+            }
         }
-
-
     }
 }
