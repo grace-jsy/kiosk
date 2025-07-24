48340d10717b0cfb313c12d3360a53180db734b8
gracy-jsy <sseo.oee@gmail.com>
2025-07-24 11:47:13 +0900

    finished

--- src/main/java/kiosklv2/MenuItem.java
+++ src/main/java/kiosklv2/MenuItem.java
@@ -13,6 +13,18 @@ public class MenuItem {
         this.description = description;
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
     public String toString() {
         return name + " | ₩ " + price + " | " + description;
     }
--- src/main/java/kiosklv3/MenuItem.java
+++ src/main/java/kiosklv3/MenuItem.java
@@ -1,4 +1,17 @@
 package kiosklv3;
 
 public class MenuItem {
+    String name;
+    double price;
+    String description;
+
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String toString()  {
+        return name + " | ₩ " + price + " | " + description;
+    }
 }
